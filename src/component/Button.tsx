100b48c8b1f68d4d47001a21d49f401d7bbb983b
Peter Nguyen <peter.quang.nguyen@gmail.com>
2023-08-28 19:12:54 +1000

    add google auth

---
+++ src/component/Button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Button(props: React.ComponentPropsWithoutRef<"button">) {
+  return (
+    <button
+      {...props}
+      className="rounded bg-blue-400 px-4 py-2 hover:bg-blue-500"
+    >
+      {props.children}
+    </button>
+  );
+}
