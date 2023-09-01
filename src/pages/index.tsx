9ef42afe0fe4d56ed76a25accba3efac1a7649a3
Peter Nguyen <peter.quang.nguyen@gmail.com>
2023-09-01 16:08:48 +1000

    update imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { PrimaryLinkButton } from "~/components/primarylinkbutton";
+import { PrimaryLinkButton } from "~/components/primarylinkbutton2";
 
 const HeroBanner = () => {
   return (
