119de8a25940ee58e45906632dcba3bc4a9cc242
Peter Nguyen <peter.quang.nguyen@gmail.com>
2023-09-22 23:35:28 +1000

    fix type error

--- src/components/landingPage/marqueeBanner.tsx
+++ src/components/landingPage/marqueeBanner.tsx
@@ -1,16 +1,13 @@
 import Image from "next/image";
+import { capitalizeString } from "~/lib/utils";
+import type { GeneratedImages } from "~/pages/generate";
 import { api } from "~/utils/api";
 import { Badge } from "../ui/badge";
-import { capitalizeString } from "~/lib/utils";
 
 const BUCKET_NAME = "ai-icon-generator2";
 
-const MarqueeImages = () => {
-  const marqueeImages = api.icons.getCommunityIcons.useQuery({ size: 12 });
-
-  if (!marqueeImages.data) return;
-
-  return marqueeImages.data.map((icon) => {
+const MarqueeImages = ({ data }: { data: GeneratedImages }) => {
+  return data.map((icon) => {
     const imageUrl = `https://${BUCKET_NAME}.s3.ap-southeast-2.amazonaws.com/${icon.id}`;
     return (
       <div key={icon.id} className="mx-2 h-[192px] w-[192px] rounded-lg border">
@@ -30,13 +27,17 @@ const MarqueeImages = () => {
 };
 
 const MarqueeBanner = () => {
+  const marqueeImages = api.icons.getCommunityIcons.useQuery({ size: 12 });
+
+  if (!marqueeImages.data) return null;
+
   return (
     <div className="relative flex overflow-x-hidden">
       <div className="flex animate-marquee whitespace-nowrap pb-16 pt-16 sm:pt-20">
-        <MarqueeImages />
+        <MarqueeImages data={marqueeImages.data} />
       </div>
       <div className="absolute top-0 flex animate-marquee2 whitespace-nowrap pb-16 pt-16 sm:pt-20">
-        <MarqueeImages />
+        <MarqueeImages data={marqueeImages.data} />
       </div>
     </div>
   );
