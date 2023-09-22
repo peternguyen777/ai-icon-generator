6f244124dd42c55eb3f33ab46bb9f43e17e801d0
Peter Nguyen <peter.quang.nguyen@gmail.com>
2023-09-22 13:29:53 +1000

    remove pagination from community tab

--- src/server/api/routers/icons.ts
+++ src/server/api/routers/icons.ts
@@ -40,31 +40,25 @@ export const iconsRouter = createTRPCRouter({
       },
     });
   }),
-  getCommunityIcons: publicProcedure
-    .input(z.object({ page: z.number().min(1) }))
-    .query(async ({ ctx, input }) => {
-      const icons = await ctx.prisma.icon.findMany({
-        skip: (input.page - 1) * 48,
-        take: 48,
-        orderBy: [
-          {
-            createdAt: "desc",
-          },
-          { id: "desc" },
-        ],
-        include: {
-          User: {
-            select: {
-              image: true,
-              name: true,
-            },
+  getCommunityIcons: publicProcedure.query(async ({ ctx }) => {
+    const icons = await ctx.prisma.icon.findMany({
+      take: 48,
+      orderBy: [
+        {
+          createdAt: "desc",
+        },
+        { id: "desc" },
+      ],
+      include: {
+        User: {
+          select: {
+            image: true,
+            name: true,
           },
         },
-      });
+      },
+    });
 
-      return icons;
-    }),
-  getTotalCommunityIcons: publicProcedure.query(async ({ ctx }) => {
-    return ctx.prisma.icon.count();
+    return icons;
   }),
 });
